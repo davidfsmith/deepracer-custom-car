93c731f04601bd7c6088421ae1b6ed329fad553a
Lars Ludvigsen <larsll@outlook.com>
2024-06-09 20:32:33 +0200

    Move end-user install files

--- install/aws-20.04/dev-stack-install-package.sh
+++ install/aws-20.04/dev-stack-install-package.sh
@@ -8,8 +8,8 @@ export DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 sudo systemctl stop deepracer-core
 
 # DeepRacer Repos
-sudo cp $DIR/files/deepracer-larsll.asc /etc/apt/trusted.gpg.d/
-sudo cp $DIR/files/aws_deepracer-community.list /etc/apt/sources.list.d/
+sudo cp $DIR/../files/deepracer-larsll.asc /etc/apt/trusted.gpg.d/
+sudo cp $DIR/../files/aws_deepracer-community.list /etc/apt/sources.list.d/
 sudo apt update
 
 # Install GPU driver
