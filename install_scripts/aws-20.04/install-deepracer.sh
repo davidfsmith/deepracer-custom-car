879cad637f0dc5171d8b9560c117a9d25d1c105b
Lars Ludvigsen <larsll@outlook.com>
2024-11-03 14:38:09 +0000

    Require run script as sudo

--- install_scripts/aws-20.04/install-deepracer.sh
+++ install_scripts/aws-20.04/install-deepracer.sh
@@ -4,19 +4,25 @@ set -e
 
 export DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. >/dev/null 2>&1 && pwd)"
 
+# Check we have the privileges we need
+if [ $(whoami) != root ]; then
+    echo "Please run this script as root or using sudo"
+    exit 0
+fi
+
 # Build the core packages
-sudo systemctl stop deepracer-core
+systemctl stop deepracer-core
 
 # DeepRacer Repos
-sudo cp $DIR/install_scripts/common/deepracer-larsll.asc /etc/apt/trusted.gpg.d/
-sudo cp $DIR/install_scripts/aws-20.04/aws_deepracer-community.list /etc/apt/sources.list.d/
-sudo apt update
+cp $DIR/install_scripts/common/deepracer-larsll.asc /etc/apt/trusted.gpg.d/
+cp $DIR/install_scripts/aws-20.04/aws_deepracer-community.list /etc/apt/sources.list.d/
+apt update
 
 # Install GPU driver
-sudo /opt/intel/openvino_2021/install_dependencies/install_NEO_OCL_driver.sh -y
+/opt/intel/openvino_2021/install_dependencies/install_NEO_OCL_driver.sh -y
 
 # Update DR
-sudo apt -y install aws-deepracer-core aws-deepracer-device-console
+apt -y install aws-deepracer-core aws-deepracer-device-console
 
 # Restart deepracer
-sudo systemctl start deepracer-core
+systemctl start deepracer-core
