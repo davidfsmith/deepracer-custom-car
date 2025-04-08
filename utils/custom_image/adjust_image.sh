ab4112c422b249c5e49216aa5755e440e2c85adc
Lars Ludvigsen <larsll@outlook.com>
2025-04-08 21:17:14 +0200

    Tools to customize the flash image

---
+++ utils/custom_image/adjust_image.sh
@@ -0,0 +1,60 @@
+# Update OpenVINO
+curl -o GPG-PUB-KEY-INTEL-SW-PRODUCTS https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS
+
+# Update ROS
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2-latest.list >/dev/null
+
+# Community Packages
+curl -sSL https://raw.githubusercontent.com/aws-deepracer-community/deepracer-custom-car/refs/heads/main/install_scripts/common/deepracer-community.asc -o /etc/apt/trusted.gpg.d/deepracer-community.asc
+curl -sSL https://raw.githubusercontent.com/aws-deepracer-community/deepracer-custom-car/refs/heads/main/install_scripts/aws-20.04/aws_deepracer-community.list -o /etc/apt/sources.list.d/aws_deepracer-community.list
+
+# Update the package cache
+apt update
+
+# Install the required packages
+apt-mark hold linux-image-deeplens linux-generic-hwe-20.04
+apt-mark manual ubuntu-standard ros-foxy-ros-base libboost-all-dev
+
+# Remove snap
+apt remove -y snapd 
+rm -rf /var/snap
+rm -rf /var/cache/snapd
+
+# Remove unnecessary packages
+apt purge -y ubuntu-desktop ubuntu-desktop-minimal ubuntu-wallpapers ros-foxy-desktop firefox-locale-en firefox fonts-indic gnome-shell gnome-keyring gnome-terminal gnome-control-center language-pack-gnome-en-base wbritish wamerican mplayer hplip gvfs
+apt autoremove -y --purge
+apt upgrade -o Dpkg::Options::="--force-overwrite" -o Dpkg::Options::='--force-confold' -y
+apt install -y --no-install-recommends python3-websocket python3-click aws-deepracer-community-device-console
+
+# Disable IPV6 on all interfaces
+echo -e -n "\nDisable IPV6\n"
+cp /etc/sysctl.conf ${backupDir}/sysctl.conf.bak
+printf "net.ipv6.conf.all.disable_ipv6 = 1" >>/etc/sysctl.conf
+
+# Grant deepracer user sudoers rights
+echo deepracer ALL=\(root\) NOPASSWD:ALL >/etc/sudoers.d/deepracer
+chmod 0440 /etc/sudoers.d/deepracer
+
+# Disable system suspend
+echo -e -n "\nDisable system suspend\n"
+systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+# Disable X on startup
+echo -e -n "\nSet console-only at startup\n"
+systemctl set-default multi-user.target
+
+# Disable network power saving
+echo -e -n "\nDisable network power saving"
+echo -e '#!/bin/sh\n/usr/sbin/iw dev mlan0 set power_save off\n' >/etc/network/if-up.d/disable_power_saving
+chmod 755 /etc/network/if-up.d/disable_power_saving
+
+# Remove Swap
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+rm /swapfile
+
+# Start SSH on startup
+echo -e -n "\nStart SSH on startup\n"
+systemctl enable ssh
---
+++ utils/custom_image/pepare.sh
@@ -0,0 +1,36 @@
+# Description: This script is used to create a custom image for the AWS DeepRacer.
+# It needs to be run as root or with sudo privileges.
+
+# Expand the filesystems onto an empty disk (e.g., /dev/sdc)
+# This script assumes that the disk is empty and does not contain any partitions.
+# The encrypted partition is assumed to be /dev/sdc3, and is mounted on /mnt/DEEPRACER.
+dcfldd if=dlrc_image_21WW09.5_tpm.img of=/dev/sdc bs=1M conv=notrunc statusinterval=1000
+cryptsetup luksOpen --allow-discards /dev/sdc3 encrypt_blk
+mkdir -p /mnt/DEEPRACER
+mount -o subvol=@ /dev/mapper/encrypt_blk /mnt/DEEPRACER
+
+# Mount the necessary filesystems to enable chroot and networking
+mount -t proc /proc /mnt/DEEPRACER/proc
+mount -t sysfs /sys /mnt/DEEPRACER/sys
+mount --bind /dev /mnt/DEEPRACER/dev
+mount --bind /dev/pts /mnt/DEEPRACER/dev/pts
+mount --bind /run /mnt/DEEPRACER/run
+
+#### RUN THE CUSTOMIZATION SCRIPT IN CHROOT ####
+cp adjust_image.sh /mnt/DEEPRACER/root/
+chmod +x /mnt/DEEPRACER/root/adjust_image.sh
+
+# Clean up
+rm /mnt/DEEPRACER/root/.bash_history
+rm /mnt/DEEPRACER/opt/aws/deepracer/password.txt
+
+# Unmount the filesystems, close the encrypted partition, and create the image
+umount /mnt/DEEPRACER/dev/pts 
+umount /mnt/DEEPRACER/dev
+umount /mnt/DEEPRACER/proc
+umount /mnt/DEEPRACER/run
+umount /mnt/DEEPRACER/sys
+umount /mnt/DEEPRACER
+cryptsetup luksClose encrypt_blk
+dcfldd if=/dev/sdc of=dlrc_image_20250407.1_tpm.img bs=1M count=15810
+
