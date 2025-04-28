b0d8e003c18c436406943d372f5fdabc1de990a2
Lars Ludvigsen <larsll@outlook.com>
2025-04-28 15:56:43 +0000

    Remove config file not needed in Raspberry Pi

--- build_scripts/build-packages.sh
+++ build_scripts/build-packages.sh
@@ -121,6 +121,7 @@ for pkg in $PACKAGES; do
                 opt/aws/deepracer/camera/installed/lib
             cp $DIR/deps/geocam-bin-armhf/files/usr/bin/mxcam opt/aws/deepracer/camera/installed/bin
             cp $DIR/install_scripts/rpi4-22.04/aws_deepracer-community.list etc/apt/sources.list.d/aws_deepracer-community.list
+            rm etc/apt/sources.list.d/aws_deepracer.list
             cp $DIR/build_scripts/files/pi/otg_eth.sh opt/aws/deepracer/util/otg_eth.sh
             cp $DIR/build_scripts/files/pi/isc-dhcp-server opt/aws/deepracer/util/isc-dhcp-server
             cp $DIR/build_scripts/files/pi/deepracer_dhcp.conf opt/aws/deepracer/util/deepracer_dhcp.conf
